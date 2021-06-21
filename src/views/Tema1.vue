11f4edc6f58622c80e4cabdd05bacf27d0174176
john <83730668+DevJohn2021@users.noreply.github.com>
2021-06-21 10:56:04 -0500

    add video- cajon color

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -79,7 +79,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="734" height="413" src="https://www.youtube.com/embed/x56FT_OVARQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
     .h3.mb-4 Ataques pasivos
