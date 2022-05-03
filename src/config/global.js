8f8b6ee8fff582fc0d3d149cdcf88a2e814ce081
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2022-05-03 12:05:33 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -163,6 +163,11 @@ export default {
         nombreRuta: 'referencias',
       },
       {
+        icono: 'fas fa-download',
+        titulo: 'Descargar material',
+        download: 'downloads/material.zip',
+      },
+      {
         icono: 'far fa-registered',
         titulo: 'Créditos',
         nombreRuta: 'creditos',
